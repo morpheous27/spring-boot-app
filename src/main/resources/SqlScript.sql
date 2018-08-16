f20a7d6aa64c81665848b635eebd149c02ba90dc
morpheous27 <nitinsaxena27@gmail.com>
2018-08-17 00:54:53 +0530

    Script file change

--- src/main/resources/SqlScript.sql
+++ src/main/resources/SqlScript.sql
@@ -1,2 +1,7 @@
+create user 'springuser'@'localhost' identified by 'springuser';
+create database springuser;
+grant all privileges on springuser to 'springuser'@'localhost';
+FLUSH PRIVILEGES;
+
 select * from notes;
 
